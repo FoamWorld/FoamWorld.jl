aa677b9bc10ffaea0d84676019b6a68b405907bc
Rratic <Rratic@163.com>
2022-02-01 21:46:45 +0800

    fil:chunk

--- src/FoamWorld.jl
+++ src/FoamWorld.jl
@@ -1,2 +1,28 @@
+module FoamWorld
 using StaticArrays
 using GtkObservables
+# 宏变量
+include("consts.jl")
+include("globals.jl")
+
+# 功能
+include("lib_collide.jl")
+include("lib_draw.jl")
+include("lib_IB.jl")
+include("lib_rand.jl")
+include("lib_string.jl")
+include("codes.jl")
+
+# 内容
+include("chunk.jl")
+include("saves.jl")
+include("tick.jl")
+
+# 细节
+include("blk/blk.jl")
+include("ent/ent.jl")
+include("gen/gen.jl")
+include("itm/itm.jl")
+include("mat/mat.jl")
+
+end # module
--- src/chunk.jl
+++ src/chunk.jl
@@ -3,6 +3,25 @@ mutable struct Chunk
 	ligs::SVector{UInt8,2048}
 	tm::UInt
 end
+function get_lig(c::Chunk,x::Int,y::Int)::UInt8
+	id=(x<<5)|(y>>1)+1
+	return (y&1==0) ? c.ligs[id]>>4 : c.ligs[id]&0xf
+end
+function set_lig(c::Chunk,x::Int,y::Int,v::UInt8)::UInt8
+	id=(x<<5)|(y>>1)+1
+	(y&1==0) ? c.ligs[id]=c.ligs[id]&0xf|(v<<4) : c.ligs[id]=c.ligs[id]&0xf0|v
+end
+function generate(c::Chunk,x::Int,y::Int;map_generator::MapGenerator)
+	map_generator.gchunk(c,x,y)
+	if haskey(sav,Pair(x,y))
+		t=@inbounds sav[Pair(x,y)]
+		for i in t
+			x0=i.first>>6
+			y0=i.first&63
+			c.bs[x0,y0]=i.second
+		end
+	end
+end
 mutable struct Dimension
 	cs::Dict{Complex{Int64},Chunk}
 end
@@ -13,4 +32,23 @@ mutable struct Game
 end
 function Game()
 	return Game(Dict{String,Dimension}(),Vector{玩家}(),Dict{String,Any}())
+end
+function pushsave(lx::Int,ly::Int,tx::Int,ty::Int,b::Block)
+	p=Pair(lx,ly)
+	if !haskey(sav,p)
+		sav[p]=Dict{Int,Block}()
+	end
+	sav[p][tx>>6|ty]=b
+end
+function makeblk(x::Int,y::Int,b::Block;player_put::Bool=false)
+	tx=x&63+1;ty=y&63+1
+	lx=x>>6;ly=y>>6
+	c=chk(dim,lx,ly)
+	onend(c.blks[tx,ty],x,y)
+	c.blks[tx,ty]=b
+	onbegin(b,x,y)
+	if player_put
+		onput_ex(b,x,y)
+	end
+	pushsave(lx,ly,tx,ty,b)
 end
