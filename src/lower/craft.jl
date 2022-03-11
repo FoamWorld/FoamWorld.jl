3b42d7dc21eec4092baab4c3f6c8805279ed808c
Rratic <Rratic@163.com>
2022-03-11 16:16:47 +0800

    fil:craft

--- src/lower/craft.jl
+++ src/lower/craft.jl
@@ -1,14 +1,20 @@
 function inner_craft(its::Vector,lin=3,col=3)
 	# 载入
-	mat=Matrix{Union{Item,Nothing}}(nothing,lin,col)
+	mat=Matrix{String}(undef,lin,col) # 辨识符
+	filln=0x0 # 非空数
 	for i in 1:lin
 		for j in 1:col
-			mat[i,j]=its[i*(col-1)+j]
+			k=i*(col-1)+j
+			mat[i,j]=its[k]
+			if !isa(mat[i,j],EI)
+				filln+=1
+			end
 		end
 	end
-	# 移到左上角
-	fl=false;mov_lin=1;mov_col=1
-	while mov_lin<lin
+# 左上角(mov_i,mov_j)
+#=
+	fl=false;mov_i=1;mov_j=1
+	while mov_i<lin
 		for j in 1:col
 			if !isa(mat[mov_lin,j],EI)
 				fl=true
@@ -16,27 +22,76 @@ function inner_craft(its::Vector,lin=3,col=3)
 			end
 		end
 		if fl break end
-		mov_lin+=1
+		mov_i+=1
 	end
 	fl=false
-	while mov_col<col
-		for i in mov_lin:lin
-			if !isa(mat[i,mov_col],EI)
+	while mov_i<col
+		for i in mov_j:lin
+			if !isa(mat[i,mov_j],EI)
 				fl=true
 				break
 			end
 		end
 		if fl break end
-		mov_col+=1
+		mov_j+=1
 	end
-	for i in mov_lin:lin
-		for j in mov_col:col
-			mat[i-mov_lin+1,j-mov_col+1]=mat[i,j]
-			mat[i,j]=nothing
+=#
+	pattern=Vector{UInt8}()
+	das=Vector{Item}(undef,filln)
+	ids=Vector{String}(undef,filln)
+	nas=Vector{String}(undef,filln)
+	cou=1
+	for i in 1:lin
+		v=0
+		for j in 1:col
+			if !isa(mat[i,j],EI)
+				das[cou]=mat[i,j]
+				ids[cou]=id(mat[i,j])
+				nas[cou]=mat[i,j].name.name
+				v+=1<<(j-1)
+				cou+=1
+			end
+		end
+		if v!=0
+			push!(pattern,v)
 		end
 	end
-	res=[]
+	res=Vector{Pair{Item,UInt8}}()
 	# 检查普通合成表
+	# [[3,1],["a1","a2","a3"],"a",2]
 	for c in data[:普通合成表]
+		if c[1]==pattern&&c[2]==ids
+			push!(res,Pair(guess_struct(c[3]),length(c)==4 ? c[4] : 1))
+		end
 	end
+	# 检查类合成表
+	# [[3,1],[]]
+	for c in data[:类合成表]
+		if c[1]!=pattern
+			continue
+		end
+		fl=false
+		tarr=Vector{Union{UInt8,Nothing}}(nothing,4)
+		for k in 1:length(c[2])>>1
+			reqid::UInt8=c[2][k<<1-1]
+			req=c[2][k<<1]
+			if req!=nas[k]
+				fl=true
+				break
+			elseif reqid!=0x0
+				if tarr[reqid]===nothing
+					tarr[reqid]=das[k].t
+				elseif tarr[reqid]!=das[k].t
+					fl=true
+					break
+				end
+			end
+		end
+		if !fl
+			str=c[3]
+			s=replace(str,"\$"=>tarr[1])
+			push!(res,Pair(guess_struct(s),length(c)==3 ? 0x1 : c[4]))
+		end
+	end
+	return res
 end
