64f0e6bc1885ee956bea6e25910f5d40504a49a6
Rratic <Rratic@163.com>
2022-03-26 19:24:33 +0800

    add:draw

---
+++ src/lib_draw.jl
@@ -0,0 +1,16 @@
+function fill_rect(ctx::GraphicsContext,x::Int=0,y::Int=0,width::Int=32,height::Int=32)
+	rectangle(ctx,x,y,width,height)
+end
+function fill_rect(ctx::GraphicsContext,x::Int=0,y::Int=0,width::Int=32,height::Int=32;color::RGB)
+	set_source_rgb(ctx,red(color),green(color),blue(color))
+	rectangle(ctx,x,y,width,height)
+end
+function fill_rect(ctx::GraphicsContext,x::Int=0,y::Int=0,width::Int=32,height::Int=32;color::RGBA)
+	set_source_rgba(ctx,red(color),green(color),blue(color),alpha(color))
+	rectangle(ctx,x,y,width,height)
+end
+function fill_text(ctx::GraphicsContext,x::Int=0,y::Int=0,text::String;color::RGB,size::Int)
+	set_source_rgb(ctx,red(color),green(color),blue(color))
+	move_to(ctx,x,y)
+	show_text(ctx,text)
+end
